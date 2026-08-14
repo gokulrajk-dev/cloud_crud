d323b6af14333caa7704ba86349c1df485b2bce3
gokulrajk-dev <kgokulraj.developer@gmail.com>
2026-08-14 19:10:40 +0530

    final commit

--- cloud_crud/settings.py
+++ cloud_crud/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 

8fde563fe38ebad98a9eb373ee6ca708a1ccd1f1
gokulrajk-dev <kgokulraj.developer@gmail.com>
2026-08-14 14:18:59 +0530

    prepare backend for render

--- cloud_crud/settings.py
+++ cloud_crud/settings.py
@@ -129,10 +129,17 @@ ASGI_APPLICATION = "cloud_crud.asgi.application"
 #     }
 # }
 
+# local db
+# DATABASES = {
+#     "default": dj_database_url.config(
+#         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+#         conn_max_age=600,
+#     )
+# }
+
 DATABASES = {
     "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
+        default=os.environ.get("DATABASE_URL")
     )
 }
 

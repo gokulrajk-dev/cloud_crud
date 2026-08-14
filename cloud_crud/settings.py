92d6a4dcc0014a7467800dfbe9d26bff64ee270f
gokulrajk-dev <kgokulraj.developer@gmail.com>
2026-08-14 13:39:04 +0530

    prepare backend for render

--- cloud_crud/settings.py
+++ cloud_crud/settings.py
@@ -122,13 +122,19 @@ ASGI_APPLICATION = "cloud_crud.asgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
+DATABASES = {
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
